9004d6e5f1be81fb3f2a3ddac51fa7f8ec3457fe
zhuchang007 <zhuchang007@139.com>
2024-02-19 07:30:06 +0800

    Reconstruct COM module, using JSerialPort directly, delete netty-serialPort.

--- Uwant-CommAdapter-Vehicle/src/main/java/uwant/vehicle/simulation/AgvSimulator.java
+++ Uwant-CommAdapter-Vehicle/src/main/java/uwant/vehicle/simulation/AgvSimulator.java
@@ -7,123 +7,133 @@
  */
 package uwant.vehicle.simulation;
 
-import io.netty.channel.ChannelHandler;
-import io.netty.handler.codec.FixedLengthFrameDecoder;
-import uwant.common.netty.tcp.ConnectionEventListener;
-import uwant.common.netty.tcp.TcpClientChannelManager;
-import uwant.common.telegrams.Response;
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortEvent;
+import com.fazecast.jSerialComm.SerialPortPacketListener;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import uwant.common.telegrams.Telegram;
-import uwant.common.vehicle.telegrams.ActionResponse;
-import uwant.common.vehicle.telegrams.NodeActionSetResponse;
-import uwant.common.vehicle.telegrams.StateResponse;
+import uwant.common.vehicle.telegrams.*;
 
-import java.util.Arrays;
-import java.util.List;
+public class AgvSimulator implements SerialPortPacketListener {
 
-public class AgvSimulator implements ConnectionEventListener<byte[]> {
-  private TcpClientChannelManager<byte[], byte[]> vehicleChannelManager;
-
-  public AgvSimulator() {
-    vehicleChannelManager =
-        new TcpClientChannelManager<>(this, this::getChannelHandlers, 5000, false);
-    vehicleChannelManager.initialize();
-    // vehicleChannelManager.connect("127.0.0.1", 2000);
-    // vehicleChannelManager.scheduleConnect("127.0.0.1", 2000, 1000);
-  }
-
-  public void connect() {
-    vehicleChannelManager.connect("127.0.0.1", 2000);
-  }
+  public AgvSimulator() {}
 
   @Override
-  public void onIncomingTelegram(byte[] paramT) {
-    if (paramT.length != StateResponse.TELEGRAM_LENGTH) return;
-    Response response;
-    switch (paramT[3]) {
-      case StateResponse.TYPE:
-        response = new StateResponse(paramT);
-        break;
-      case ActionResponse.TYPE:
-        response = new ActionResponse(paramT);
-        break;
-      case NodeActionSetResponse.TYPE:
-        response = new NodeActionSetResponse(paramT);
-        break;
-      default:
-        System.out.println("Unknown Type!");
-        response = null;
-        break;
-    }
-    if (response != null) {
-      System.out.println("Incoming request: " + response.getHexRawContent());
-    }
-  }
+  public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_RECEIVED; }
 
   @Override
-  public void onConnect(Object object) {
-    System.out.println("connect successful!");
-  }
+  public int getPacketSize() { return Telegram.TELEGRAM_LENGTH; }
+
+  private static SerialPort comPort;
 
   @Override
-  public void onFailedConnectionAttempt() {}
+  public void serialEvent(SerialPortEvent event)
+  {
+    byte[] recvTelegram = event.getReceivedData();
 
-  @Override
-  public void onDisconnect() {}
-
-  @Override
-  public void onIdle() {}
-
-  public void sendTelegram(byte[] request) {
-    if (!isVehicleConnected()) {
+    if (recvTelegram.length != Telegram.TELEGRAM_LENGTH) {
+      System.out.println("Receive data wrong length! ");
       return;
     }
-    vehicleChannelManager.send(request);
-  }
 
-  public synchronized boolean isVehicleConnected() {
-    return vehicleChannelManager != null && vehicleChannelManager.isConnected();
-  }
+    if (((recvTelegram[2] ^ recvTelegram[Telegram.TELEGRAM_LENGTH - 1]) & 0xff) != 0xff) {
+      System.out.println("Receive head and tail do not equals to '0xff'! ");
+      return;
+    }
 
-  private List<ChannelHandler> getChannelHandlers() {
-    return Arrays.asList(
-        // new LengthFieldBasedFrameDecoder(getMaxTelegramLength(), 1, 1, 2, 0),
-        new FixedLengthFrameDecoder(NodeActionSetResponse.TELEGRAM_LENGTH),
-        // new VehicleTelegramDecoder(this),
-        new TelegramDecoderClient(this),
-        new TelegramEncoderClient());
+    if (recvTelegram[3] == StateResponse.TYPE) {
+    }
+    else if (recvTelegram[3] == ActionRequest.TYPE) {
+      ActionRequest actionRequest = new ActionRequest(recvTelegram);
+      System.out.println("receive ActionRequest: " + actionRequest.getHexRawContent());
+      byte[] newData = new byte[Telegram.TELEGRAM_LENGTH];
+      newData[0] = recvTelegram[0];
+      newData[1] = recvTelegram[1];
+      newData[2] = (byte)(recvTelegram[2] ^ 0x80);
+      newData[3] = ActionResponse.TYPE;
+      newData[4] = 1;
+      newData[20] = (byte) (newData[3] + newData[4]);
+      newData[21] = (byte) (0xff - newData[2]);
+      ActionResponse actionResponse = new ActionResponse(newData);
+      try {
+        TimeUnit.MILLISECONDS.sleep(200);
+      }
+      catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      comPort.writeBytes(actionResponse.getRawContent(),Telegram.TELEGRAM_LENGTH);
+      System.out.println("send actionResponse: " + actionResponse.getHexRawContent());
+    }
+    else if (recvTelegram[3] == NodeActionRequest.TYPE) {
+      NodeActionRequest nodeActionRequest = new NodeActionRequest(recvTelegram);
+      System.out.println("receive nodeActionRequest: " + nodeActionRequest.getHexRawContent());
+      byte[] newData = new byte[Telegram.TELEGRAM_LENGTH];
+      newData[0] = recvTelegram[0];
+      newData[1] = recvTelegram[1];
+      newData[2] = (byte)(recvTelegram[2] ^ 0x80);
+      newData[3] = NodeActionResponse.TYPE;
+      newData[4] = 1;
+      newData[20] = (byte) (newData[3] + newData[4]);
+      newData[21] = (byte) (0xff - newData[2]);
+      NodeActionResponse nodeActionResponse = new NodeActionResponse(newData);
+      try {
+        TimeUnit.MILLISECONDS.sleep(200);
+      }
+      catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      comPort.writeBytes(nodeActionResponse.getRawContent(),Telegram.TELEGRAM_LENGTH);
+      System.out.println("send nodeActionResponse: " + nodeActionResponse.getHexRawContent());
+    }
   }
 
   public static void main(String[] args) throws InterruptedException {
     AgvSimulator agvSimulator = new AgvSimulator();
-    int count = 3;
-    while (count-- > 0) {
-      agvSimulator.connect();
-      Thread.sleep(1000);
-    }
-    Thread.sleep(1000);
-    byte[] stateResponse = new byte[20];
-    // 小车编号
-    stateResponse[0] = (byte) (0x80 + 0x01);
-    // 命令字
-    stateResponse[1] = 0x01;
-    // 数据
-    stateResponse[4] = 0x02;
-    stateResponse[5] = 0x03;
-    stateResponse[6] = 0x04;
-    // 校验和
-    stateResponse[18] = Telegram.getCheckSum(stateResponse, 1, 18);
-    // 帧尾
-    stateResponse[19] = (byte) (0xff - stateResponse[0]);
 
-    StateResponse stateResponse1 = new StateResponse(stateResponse);
-    System.out.println("发送：" + stateResponse1.getHexRawContent());
+    byte[] rawStateResponse = new byte[22];
+    rawStateResponse[0] = 0x00;
+    rawStateResponse[1] = 0x01;
+    // 小车编号
+    rawStateResponse[2] = (byte) (0x80 + 0x01);
+    // 命令字
+    rawStateResponse[3] = 0x01;
+    // 数据
+    rawStateResponse[7] = 0x01;
+    rawStateResponse[8] = 0x01;
+    // 校验和
+    rawStateResponse[20] = Telegram.getCheckSum(rawStateResponse, 3, 19);
+    // 帧尾
+    rawStateResponse[21] = (byte) (0xff - rawStateResponse[2]);
+
+    StateResponse stateResponse = new StateResponse(rawStateResponse);
+
+    SerialPort[] comPorts = SerialPort.getCommPorts();
+//    for (int i = 0; i < comPorts.length; i++) {
+//      System.out.println("串口名称" + comPorts[i].getSystemPortName() +"\n");
+//    }
+    comPort = comPorts[3];
+    comPort.setBaudRate(57600);
+    comPort.openPort();
+    comPort.addDataListener(agvSimulator);
+
+//    comPort.removeDataListener();
+//    comPort.closePort();
+
+    System.out.println("send stateResponse every second：" + stateResponse.getHexRawContent());
+    ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(1);
+    executorService.scheduleAtFixedRate(()->{
+      comPort.writeBytes(stateResponse.getRawContent(),Telegram.TELEGRAM_LENGTH);
+    },0, 1000, TimeUnit.MILLISECONDS);
 
     while (true) {
-      agvSimulator.sendTelegram(stateResponse1.getRawContent());
-      Thread.sleep(500);
+      Thread.sleep(1000);
     }
-    //    while (true) {
-    //      System.out.println(tcpClient.isVehicleConnected() + "\r\n");
-    //    }
   }
+
 }
+
+
