c41821a9b04b4bf6e0f86660b0273d9fe0c0e21d
zhuchang007 <zhuchang007@139.com>
2024-06-24 08:27:28 +0800

    use jdk21 and fix some format issues

--- Uwant-Common/src/main/java/uwant/common/telegrams/RequestResponseMatcher.java
+++ Uwant-Common/src/main/java/uwant/common/telegrams/RequestResponseMatcher.java
@@ -13,7 +13,7 @@ import static java.util.Objects.requireNonNull;
 import java.util.Optional;
 import java.util.Queue;
 import javax.annotation.Nonnull;
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
